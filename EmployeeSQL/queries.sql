b7e815b34b3b46fbcb6cc310716902b182554c66
Sydni <sydnimpierce@gmail.com>
2021-05-15 10:52:22 -0400

    Modified analysis 4 to complete analysis 6

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -31,3 +31,12 @@ WHERE first_name = 'Hercules'
 AND
 	(SELECT substring
 		(last_name for 1) = 'B');
+		
+-- 6: List emp #, last + first name, dept name for Sales dept
+Select de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+LEFT JOIN departments AS d
+ON (de.dept_no=d.dept_no)
+	LEFT JOIN employees AS e
+	ON (de.emp_no=e.emp_no)
+	WHERE d.dept_name = 'Sales';

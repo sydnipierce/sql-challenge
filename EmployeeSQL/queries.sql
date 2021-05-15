7d2cb2268b0beddf446c60a19cb2636e67d9ff2e
Sydni <sydnimpierce@gmail.com>
2021-05-15 10:55:06 -0400

    Modified analysis 6 to complete 7

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -40,3 +40,12 @@ ON (de.dept_no=d.dept_no)
 	LEFT JOIN employees AS e
 	ON (de.emp_no=e.emp_no)
 	WHERE d.dept_name = 'Sales';
+	
+-- 7: Modify 6 to include Sales AND Development
+Select de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+LEFT JOIN departments AS d
+ON (de.dept_no=d.dept_no)
+	LEFT JOIN employees AS e
+	ON (de.emp_no=e.emp_no)
+	WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';

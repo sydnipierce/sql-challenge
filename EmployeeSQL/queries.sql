27826f7e0ea12ca8732c9eb6a37cc37151ec2cd6
Sydni <sydnimpierce@gmail.com>
2021-05-15 10:19:44 -0400

    Completed data analysis 4

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,14 +1,14 @@
--- List emp #, last + first name, sex, salary
+-- 1: List emp #, last + first name, sex, salary
 Select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees AS e
 LEFT JOIN salaries AS s ON
 e.emp_no=s.emp_no;
 
--- List first + last name, hire date for hire year = 1986
+-- 2: List first + last name, hire date for hire year = 1986
 Select first_name, last_name, hire_date FROM employees
 WHERE DATE_PART('year', hire_date) = 1986;
 
--- List dept # + name, manager emp # + last + first name
+-- 3: List dept # + name, manager emp # + last + first name
 Select dm.dept_no, dm.emp_no, d.dept_name, e.last_name, e.first_name
 FROM dept_manager AS dm
 LEFT JOIN departments AS d
@@ -16,3 +16,12 @@ ON (dm.dept_no=d.dept_no)
 	LEFT JOIN employees AS e
 	ON (dm.emp_no=e.emp_no);
 
+-- 4: List dept name, emp #, last name, first name
+Select de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+LEFT JOIN departments AS d
+ON (de.dept_no=d.dept_no)
+	LEFT JOIN employees AS e
+	ON (de.emp_no=e.emp_no);
+	
+-- 5: 
